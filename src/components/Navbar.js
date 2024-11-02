0b9b24bc17a376422249bb11d0617bc1d8a62b70
aliakbarzn <s.a.zabihnejad@gmail.com>
2024-11-02 09:55:03 +0330

    css improve

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,8 @@
 import React, { useState } from 'react';
 import { useTheme } from '../contexts/ThemeContext';
 import './Navbar.css'
-import icon from '../assets/icons/brightness_6_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg'
+import lightMode from '../assets/icons/brightness_6_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg'
+import darkMode from '../assets/icons/dark_mode.svg'
 
 const Navbar = () => {
     const {isDarkMode, toggleTheme} = useTheme()
@@ -12,8 +13,8 @@ const Navbar = () => {
             <h1 className="navbar-title">Where in the world?</h1>
             <div className="dark-mode-toggle" onClick={toggleTheme}>
                 <img
-                    src={icon}
-                    style={{ filter: isDarkMode ? 'invert(100%)' : 'invert(20%)' }}
+                    src={isDarkMode ? darkMode : lightMode}
+                    // style={{ filter: isDarkMode ? 'invert(100%)' : 'invert(20%)' }}
                 />
                 <span>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</span>
             </div>
